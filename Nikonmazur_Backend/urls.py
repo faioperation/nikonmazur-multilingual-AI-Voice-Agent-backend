55d594a7ce1d101af0efb5bf2190f2e1852cc4f8
aru-01 <arifu5758@gmail.com>
2026-06-28 17:07:08 +0600

    Complete v.1.0.0

--- Nikonmazur_Backend/urls.py
+++ Nikonmazur_Backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
-from debug_toolbar.toolbar import debug_toolbar_urls
+
+# from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import api_root_view
@@ -33,7 +34,8 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-] + debug_toolbar_urls()
+]
+# + debug_toolbar_urls()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
